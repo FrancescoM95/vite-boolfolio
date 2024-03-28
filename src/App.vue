24d3e02163594048b0881deaf3e5040ac7095b4a
FrancescoM95 <mon95fran@gmail.com>
2024-03-28 15:40:50 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import axios from 'axios';
+const endpoint = 'http://localhost:8000/api/projects/';
+
+export default{
+  name: 'Boolfolio',
+  components: {AppHeader},
+  
+}
+</script>
+
+<template>
+<AppHeader />
+<h1>Boolfolio</h1>
+
+</template>
+
+<style>
+</style>
