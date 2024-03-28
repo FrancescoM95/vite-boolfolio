e58f48da5a9b1c237ddcc224ea93712c63f08257
FrancescoM95 <mon95fran@gmail.com>
2024-03-28 15:41:56 +0100

    milestone 4

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,29 @@ const endpoint = 'http://localhost:8000/api/projects/';
 export default{
   name: 'Boolfolio',
   components: {AppHeader},
-  
+  data: () => ({projects: []}),
+  methods:{
+    fetchProject(){
+      axios.get(endpoint).then(res => {
+        console.log(res.data);
+       
+      })
+    }
+  },
+  created(){
+    this.fetchProject();
+  }
 }
 </script>
 
 <template>
 <AppHeader />
 <h1>Boolfolio</h1>
-
+<ul>
+  <li v-for="project in projects" :key="project.id">
+    <h4 v-text="project.title"></h4>
+  </li>
+</ul>
 </template>
 
 <style>
