b4a15eaa2436f473f13223ceaf71d706726701fe
FrancescoM95 <mon95fran@gmail.com>
2024-04-02 17:59:05 +0200

    added page 404

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,13 @@
+<script>
+export default{
+  name: 'NotFoundPage',
+};
+</script>
+
+<template>
+
+<div class="vh-100 d-flex justify-content-center align-items-center">
+<h3 class="text-white"> 404 | Page not found</h3>
+</div>
+
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from '../pages/HomePage.vue';
 
 import ProjectShow from '../pages/ProjectShow.vue';
+import NotFoundPage from '../pages/NotFoundPage.vue';
 
 export default createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: HomePage },
         { path: '/projects/:id', component: ProjectShow, name: 'project-show' },
+        { path: '/:pathMatch(.*)*', component: NotFoundPage },
     ]
 })
